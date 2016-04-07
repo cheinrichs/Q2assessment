61c9f9b197470847db747ac063808e2beff3b4d9
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-04-07 14:27:56 -0600

    changed some styling, and moved global styles to the layout.hbs file

--- routes/authors.js
+++ routes/authors.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('respond with a resource');
+  res.render('authors');
 });
 
 module.exports = router;
