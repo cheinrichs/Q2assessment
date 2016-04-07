7db05c0c713fd1610eddff1758422cc696b17a4a
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-04-07 14:40:16 -0600

    Added new styling

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('books');
+  res.render('books', {books});
 });
 
 module.exports = router;
