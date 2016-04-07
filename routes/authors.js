99a1ece5bd81922a12c6d672256aac7a0029875a
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-04-07 16:35:28 -0600

    added functionality so users can now list all authors

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var queries = require('../lib');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('authors');
+  queries.listAllAuthors().then(function(authors){
+    res.render('authors', { authors: authors });
+  });
+
 });
 
 module.exports = router;
