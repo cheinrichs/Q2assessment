d1312a92fbdb661202905295ce30304be4bd2bae
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-04-07 17:45:57 -0600

    wip

--- routes/authors.js
+++ routes/authors.js
@@ -6,14 +6,12 @@ var queries = require('../lib');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   queries.listAllAuthors().then(function(authors){
-    console.log(authors);
     res.render('authors', { authors: authors });
   });
 });
 
 router.get('/:id', function(req, res, next){
   queries.showAuthorById(req.params.id).then(function(author){
-    console.log(author);
     res.render('author', { authors: author });
   });
 });
