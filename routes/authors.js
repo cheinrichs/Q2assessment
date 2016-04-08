2f8392f7812d5d34a25415f800e151515471bae2
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-04-08 10:41:05 -0600

    The user can now delete Authors

--- routes/authors.js
+++ routes/authors.js
@@ -14,6 +14,20 @@ router.get('/new', function(req, res, next){
   res.render('newAuthor');
 });
 
+router.get('/delete/:id', function(req, res, next){
+  queries.showAuthorById(req.params.id).then(function(authors){
+    console.log(authors);
+    res.render('deleteAuthor', { author: authors.author, books: authors.books });
+  });
+});
+
+
+router.get('/deleteAuthor/:id', function(req, res, next){
+  queries.deleteAuthor(req.params.id).then(function(author){
+    res.redirect('/authors');
+  });
+});
+
 router.post('/createNewAuthor', function(req, res, next){
   queries.createAuthor(req.body.firstName, req.body.lastName, req.body.biography, req.body.portraitUrl).then(function(id){
     res.redirect('/authors');
